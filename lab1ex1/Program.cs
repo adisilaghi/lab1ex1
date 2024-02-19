88cd181eb2d2ca45d90232e0b75de2d70c5f19db
Administrator <Administrator@DESKTOP-IKD2D8I>
2024-02-19 20:43:24 +0200

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Ex1 Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+paralelipiped dreptunghic, va afisa volumul lui*/
+
+
+Console.WriteLine("Introduceti lungimea");
+int lungime = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introduceti latime");
+int latime = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introduceti inaltime");
+int inaltime = Convert.ToInt32(Console.ReadLine());
+
+int volum = (lungime * latime * inaltime);
+Console.WriteLine("Volumul paralelipedului este:"+volum);
+
